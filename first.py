2ec1d975d2000947daa8a3231d7de67a9019686f
sourav <souravsainioffice8619-cloud>
2026-01-29 13:01:12 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,9 @@
+def fun(name , **data):
+    print("Name:", name)
+    for key , value in data.items():
+        print(f"{key} : {value}")
+    print("this output is using .get() method",data.get("age"))
+    print("this output is using .get() method",data.get("city"))
+fun("Alice", age=30, city="New York", profession="Engineer")
+# **data is used to pass a variable number of keyword arguments to a function.
+# **data collects these keyword arguments into a dictionary named 'data'.
